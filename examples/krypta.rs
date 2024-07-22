4ef024eab07f292025aba69c8b33dc1aaa5d217f
Molive <moliveofscratch@gmail.com>
2024-07-22 17:55:14 +0100

    format

--- examples/krypta.rs
+++ examples/krypta.rs
@@ -23,7 +23,8 @@ pub struct MouseLight;
 #[derive(Component)]
 pub struct Movable;
 
-fn main() {
+fn main()
+{
     // Basic setup.
     App::new()
         .insert_resource(ClearColor(Color::srgba_u8(0, 0, 0, 0)))
@@ -84,7 +85,8 @@ fn setup(
     camera_targets: Res<CameraTargets>,
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlasLayout>>,
-) {
+)
+{
     // Utility functions to compute Z coordinate for floor and ground objects.
     let get_floor_z = |y| -> f32 { Z_BASE_FLOOR - y / SCREEN_SIZE.1 };
     let get_object_z = |y| -> f32 { Z_BASE_OBJECTS - y / SCREEN_SIZE.1 };
@@ -186,7 +188,7 @@ fn setup(
                         },
                         TextureAtlas {
                             layout: floor_atlas.clone(),
-                            index: id as usize,
+                            index:  id as usize,
                         },
                     ))
                     .insert(RenderLayers::from_layers(CAMERA_LAYER_FLOOR))
@@ -316,7 +318,7 @@ fn setup(
                             },
                             TextureAtlas {
                                 layout: wall_atlas.clone(),
-                                index: id as usize,
+                                index:  id as usize,
                             },
                         ))
                         .insert(RenderLayers::from_layers(CAMERA_LAYER_WALLS))
@@ -386,7 +388,7 @@ fn setup(
                         },
                         TextureAtlas {
                             layout: texture_atlas_handle.clone(),
-                            index: candle_rect_1,
+                            index:  candle_rect_1,
                         },
                     ))
                     .insert(RenderLayers::from_layers(CAMERA_LAYER_OBJECTS))
@@ -421,7 +423,7 @@ fn setup(
                         },
                         TextureAtlas {
                             layout: texture_atlas_handle.clone(),
-                            index: candle_rect_2,
+                            index:  candle_rect_2,
                         },
                     ))
                     .insert(RenderLayers::from_layers(CAMERA_LAYER_OBJECTS))
@@ -456,7 +458,7 @@ fn setup(
                         },
                         TextureAtlas {
                             layout: texture_atlas_handle.clone(),
-                            index: candle_rect_3,
+                            index:  candle_rect_3,
                         },
                     ))
                     .insert(RenderLayers::from_layers(CAMERA_LAYER_OBJECTS))
@@ -491,7 +493,7 @@ fn setup(
                         },
                         TextureAtlas {
                             layout: texture_atlas_handle.clone(),
-                            index: candle_rect_4,
+                            index:  candle_rect_4,
                         },
                     ))
                     .insert(RenderLayers::from_layers(CAMERA_LAYER_OBJECTS))
@@ -525,7 +527,7 @@ fn setup(
                         },
                         TextureAtlas {
                             layout: texture_atlas_handle.clone(),
-                            index: tomb_rect_1,
+                            index:  tomb_rect_1,
                         },
                     ))
                     .insert(RenderLayers::from_layers(CAMERA_LAYER_OBJECTS))
@@ -559,7 +561,7 @@ fn setup(
                         },
                         TextureAtlas {
                             layout: texture_atlas_handle.clone(),
-                            index: tomb_rect_1,
+                            index:  tomb_rect_1,
                         },
                     ))
                     .insert(RenderLayers::from_layers(CAMERA_LAYER_OBJECTS))
@@ -594,7 +596,7 @@ fn setup(
                         },
                         TextureAtlas {
                             layout: texture_atlas_handle.clone(),
-                            index: sewerage_rect_1,
+                            index:  sewerage_rect_1,
                         },
                     ))
                     .insert(RenderLayers::from_layers(CAMERA_LAYER_FLOOR)) // Add to floor
@@ -745,10 +747,10 @@ fn setup(
             -1163.2,
             "outdoor_light_9",
             OmniLightSource2D {
-                intensity: 1.2,
-                falloff: Vec3::new(50.0, 40.0, 0.03),
-                color: Color::srgb_u8(0, 206, 94),
-                jitter_intensity: 0.7,
+                intensity:          1.2,
+                falloff:            Vec3::new(50.0, 40.0, 0.03),
+                color:              Color::srgb_u8(0, 206, 94),
+                jitter_intensity:   0.7,
                 jitter_translation: 3.0,
             },
         ));
@@ -759,10 +761,10 @@ fn setup(
             -1210.0,
             "outdoor_light_10",
             OmniLightSource2D {
-                intensity: 1.2,
-                falloff: Vec3::new(50.0, 40.0, 0.03),
-                color: Color::srgb_u8(0, 206, 94),
-                jitter_intensity: 0.7,
+                intensity:          1.2,
+                falloff:            Vec3::new(50.0, 40.0, 0.03),
+                color:              Color::srgb_u8(0, 206, 94),
+                jitter_intensity:   0.7,
                 jitter_translation: 3.0,
             },
         ));
@@ -801,7 +803,7 @@ fn setup(
     // Add skylight light.
     commands.spawn((
         SkylightLight2D {
-            color: Color::srgb_u8(93, 158, 179),
+            color:     Color::srgb_u8(93, 158, 179),
             intensity: 0.025,
         },
         Name::new("global_skylight"),
@@ -894,7 +896,8 @@ fn system_control_mouse_light(
     query_cameras: Query<(&Camera, &GlobalTransform), With<SpriteCamera>>,
     mouse: Res<ButtonInput<MouseButton>>,
     keyboard: Res<ButtonInput<KeyCode>>,
-) {
+)
+{
     let mut rng = thread_rng();
 
     // We only need to iter over first camera matched.
